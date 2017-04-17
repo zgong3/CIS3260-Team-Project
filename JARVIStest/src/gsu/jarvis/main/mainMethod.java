5fff0f7b92cb4b18fc1a5752927b7e143bfc1112
Zhanpeng Gong <henry7412369@gmail.com>
2017-04-17 17:04:20 -0400

    Update mainMethod.java

--- JARVIStest/src/gsu/jarvis/main/mainMethod.java
+++ JARVIStest/src/gsu/jarvis/main/mainMethod.java
@@ -4,17 +4,22 @@ import gsu.jarvis.ui.MainWindow;
 import gsu.jarvis.ui.TextPanel;
 import gsu.jarvis.ui.VoicePanel;
 
+/**
+ * @author Zhanpeng Gong
+ *	Main method class
+ */
 public class mainMethod {
 	
 	/**Main method to start application**/
 	public static void main(String[] args) {
 		
-	
+		//Create text panel
 		TextPanel panelText = new TextPanel();
 		
+		//Create voice panel
 		VoicePanel panelVoice = new VoicePanel();
 		
-		
+		//Start main window
 		new MainWindow(panelText, panelVoice);
 		
 		
