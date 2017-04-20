08c8a534981aa29f3177d4b6f658ea377920837b
Zhanpeng Gong <henry7412369@gmail.com>
2017-04-20 15:18:31 -0400

    Update Spotify.java

--- JARVIStest/src/gsu/jarvis/text/Spotify.java
+++ JARVIStest/src/gsu/jarvis/text/Spotify.java
@@ -4,12 +4,20 @@ package gsu.jarvis.text;
 public class Spotify {
 
 	public void turnOn() {
+		
 		Decision.setResponse("Turning Spotify On");
+		
 	}
+	
 	public void turnOff() {
+		
 		Decision.setResponse("Turning Spotify Off");
+		
 	}
+	
 	public void playSong() {
+		
 		Decision.setResponse("Ok, the song is new playing");
+		
 	}
 }
