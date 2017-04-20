7f2804e57f15bfe2dbc1c272b4230e1e47ad6593
Zhanpeng Gong <henry7412369@gmail.com>
2017-04-20 15:19:13 -0400

    Update TV.java

--- JARVIStest/src/gsu/jarvis/text/TV.java
+++ JARVIStest/src/gsu/jarvis/text/TV.java
@@ -4,25 +4,57 @@ package gsu.jarvis.text;
 public class TV {
 	
 	public void turnON() {
-		Decision.setResponse("OK, the TV is now turned on");
-		}
+		
+		Decision.setResponse("OK, the TV is now turned on!");
+		
+	}
+	
 	public void turnOFF() {
-		Decision.setResponse("OK, the TV is now turned off");
-	}
-	public void turnChannelUP(int channel) {
 
-	}
-	public void turnChannelDown(int channel) {
+		Decision.setResponse("OK, the TV is now turned off!");
 	
 	}
+	
+	public void turnChannelUP() {
+		
+		Decision.setResponse("OK, turn to the next channel!");
+	
+	}
+	
+	public void turnChannelDown() {
+		
+		Decision.setResponse("OK, turn back one channel!");
+	
+	}
+	
+	public void turnToChannel(int channel) {
+		
+		if(channel != -1){
+		
+			Decision.setResponse("OK, switch to channel " + channel + ".");
+		
+		}else
+		
+			Decision.setResponse("This channel does not exist.");	
+	
+	}
+	
 	public void mute() {
-		Decision.setResponse("OK, the TV is on mute now");
+		
+		Decision.setResponse("OK, the TV is on mute now!");
+	
 	}
+	
 	public void turnVolumeUp() {
-		Decision.setResponse("The volume is turned up");
+		
+		Decision.setResponse("The volume has turned up.");
+	
 	}
+	
 	public void turnVolumeDown() {
-		Decision.setResponse("The volume is turned down");
+		
+		Decision.setResponse("The volume has turned down.");
+	
 	}
 	
 }
