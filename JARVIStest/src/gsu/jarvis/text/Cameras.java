98cf185d5c69889eafb406a2d6018fc39dd6582e
Zhanpeng Gong <henry7412369@gmail.com>
2017-04-20 15:16:14 -0400

    Update Cameras.java

--- JARVIStest/src/gsu/jarvis/text/Cameras.java
+++ JARVIStest/src/gsu/jarvis/text/Cameras.java
@@ -4,9 +4,25 @@ package gsu.jarvis.text;
 public class Cameras {
 
 	public void turnCameraON() {
+		
 		Decision.setResponse("Ok! cameras are now turned on");
+		
 	}
+	
 	public void turnCameraOFF() {
+		
 		Decision.setResponse("Cameras are now turned off");
+		
+	}
+	
+	public void zoomIn() {
+		
+		Decision.setResponse("The camera is zoomed in");
+	}
+	
+	public void zoomOut() {
+		
+		Decision.setResponse("The camera is zoomed out");
+		
 	}
 }
