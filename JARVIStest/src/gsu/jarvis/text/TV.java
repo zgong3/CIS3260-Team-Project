16ec446e776bfe4b887fa2baaea42ce6991faaab
Zhanpeng Gong <henry7412369@gmail.com>
2017-04-20 15:34:47 -0400

    Update TV.java

--- JARVIStest/src/gsu/jarvis/text/TV.java
+++ JARVIStest/src/gsu/jarvis/text/TV.java
@@ -3,31 +3,31 @@ package gsu.jarvis.text;
 
 public class TV {
 	
-	public void turnON() {
+	protected void turnON() {
 		
 		Decision.setResponse("OK, the TV is now turned on!");
 		
 	}
 	
-	public void turnOFF() {
+	protected void turnOFF() {
 
 		Decision.setResponse("OK, the TV is now turned off!");
 	
 	}
 	
-	public void turnChannelUP() {
+	protected void turnChannelUP() {
 		
 		Decision.setResponse("OK, turn to the next channel!");
 	
 	}
 	
-	public void turnChannelDown() {
+	protected void turnChannelDown() {
 		
 		Decision.setResponse("OK, turn back one channel!");
 	
 	}
 	
-	public void turnToChannel(int channel) {
+	protected void turnToChannel(int channel) {
 		
 		if(channel != -1){
 		
@@ -39,19 +39,19 @@ public class TV {
 	
 	}
 	
-	public void mute() {
+	protected void mute() {
 		
 		Decision.setResponse("OK, the TV is on mute now!");
 	
 	}
 	
-	public void turnVolumeUp() {
+	protected void turnVolumeUp() {
 		
 		Decision.setResponse("The volume has turned up.");
 	
 	}
 	
-	public void turnVolumeDown() {
+	protected void turnVolumeDown() {
 		
 		Decision.setResponse("The volume has turned down.");
 	
