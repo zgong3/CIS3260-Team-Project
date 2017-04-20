cb11ec5575fc24fd326a3f3ddb1a6a377dc20f9b
Zhanpeng Gong <henry7412369@gmail.com>
2017-04-20 15:19:37 -0400

    Update Toaster.java

--- JARVIStest/src/gsu/jarvis/text/Toaster.java
+++ JARVIStest/src/gsu/jarvis/text/Toaster.java
@@ -3,10 +3,15 @@ package gsu.jarvis.text;
 public class Toaster {
 
 	public void toasterON() {
+		
 		Decision.setResponse("Ok! turning toaster on");
+		
 	}
+	
 	public void toasterOFF() {
+		
 		Decision.setResponse("Turning toaster off");
+		
 	}
 	
 }
