961c56ccb1c947e4840aa0825dfe94ad64bf35f4
alecxu1 <axu2@student.gsu.edu>
2017-04-24 20:02:30 -0400

    Updated Speaker.java
    I added some commands to the speaker class, I am going add the if/else code into the Decision Class as well

--- JARVIStest/src/gsu/jarvis/text/Speaker.java
+++ JARVIStest/src/gsu/jarvis/text/Speaker.java
@@ -3,4 +3,34 @@ package gsu.jarvis.text;
 
 public class Speaker {
   
+  protected void turnSpeakerOn() {
+    
+     Decision.setResponse("Speakers are on! ");
+  
+}
+  
+  protected void turnSpeakeroff() {
+    
+      Decision.setResponse("Speakers are off! ")
+        
+}
+
+  protected void speakerVolumeLow() {
+    
+      Decision.setResponse("Speaker volume is on low ");
+    
+}
+  
+  protected void speakerVolumeMed() {
+    
+      Decision.setResponse("Speaker volume is on medium ");
+    
+}
+  
+  protected voide speakerVolumeHigh() {
+    
+      Decision.setResponse("Speaker volume is on high ");
+  
+}
+  
 }
