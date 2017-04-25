b74a87e4c5ade9f1eccdcffa903f71e400a74458
Zhanpeng Gong <henry7412369@gmail.com>
2017-04-25 16:20:51 -0400

    Add files via upload

---
+++ JARVIStest/src/gsu/jarvis/text/Greeting.java
@@ -0,0 +1,42 @@
+package gsu.jarvis.text;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Greeting {
+	
+	protected void howAreYou(){
+		
+		Decision.setResponse("I'm fine, thank you.");
+		
+	}
+	
+	protected void whatsYourName(){
+		
+		Decision.setResponse("My name is JARVIS, how can I help you?");
+		
+	}
+	
+	protected void hi(){
+		
+		Decision.setResponse("Hi! My name is JARVIS, how can I help you?");
+		
+	}
+	
+	protected void nowTime(){
+		
+		Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");	
+		Decision.setResponse("Current time is: "+sdf.format(cal.getTime()));
+		
+	}
+	
+protected void nowDate(){
+		
+		Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");	
+		Decision.setResponse("Today's date is: "+sdf.format(cal.getTime()));
+		
+	}
+
+}
